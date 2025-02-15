ceba26556f6a4020136614789e45d0bb9da3c57d
kannan <srikannan251200@gmail.com>
2025-02-15 16:11:11 +0530

    user added

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class User {
+    private int id;
+    private String name;
+    private String brand;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 }
